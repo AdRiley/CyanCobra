ba934ea607ef33a472bfa026137cbbbfb827178e
AdRiley <riley.ad@gmail.com>
2014-12-15 06:08:02 -0700

    Room maker adds a door

--- src/RoomMaker.cpp
+++ src/RoomMaker.cpp
@@ -1,14 +1,29 @@
 #include"RoomMaker.h"
 
 RoomMaker::RoomMaker(GameMap& gm) : m_GameMap(gm) {}
-void RoomMaker::MakeRoom(Point buildPoint, unsigned int size)
+void RoomMaker::MakeRoom(Point buildPoint, unsigned int size, unsigned int NumberOfDoors /*= 0*/)
 {
 	m_GameMap.SetTile(buildPoint, Tile::Floor);
 
+	std::vector<Point> availablePoints;
 	for (auto p : buildPoint.AdjPoints())
 		if (m_GameMap.GetTile(p) == Tile::Empty)
+			availablePoints.push_back(p);
+
+	unsigned int i = 0;
+	for (auto p : availablePoints)
+	{
+		if (i < NumberOfDoors)
+		{
+			m_GameMap.SetTile(p, Tile::ClosedDoor); 
+			++i;
+		}
+		else
+		{
 			m_GameMap.SetTile(p, Tile::Wall); 
-			
+		}
+	}
+
 	for (auto p : buildPoint.DiagPoints())
 		m_GameMap.SetTile(p, Tile::Wall); 
 
--- src/RoomMakerTester.cpp
+++ src/RoomMakerTester.cpp
@@ -66,3 +66,37 @@ SCENARIO("A room request of size 1 makes a room")
 	    }
 	}
 }
+
+SCENARIO("A room request of size 1 makes a room with doors")
+{
+	GIVEN("A map with a build point with a door left and a RoomMaker")
+	{
+	    GameMap gm{2, 2};
+	    Point buildPoint{1, 1};
+	    gm.SetTile(buildPoint.Left(), Tile::ClosedDoor);
+	    RoomMaker rm(gm);
+	    WHEN("We make a room of size 1 at point p with 1 door")
+	    {
+			rm.MakeRoom(buildPoint, 1, 1);
+			THEN("Point p is floor")
+			{
+				REQUIRE(gm.GetTile(buildPoint) == Tile::Floor);
+			}
+			THEN("The door is still a door")
+			{
+				REQUIRE(gm.GetTile(buildPoint.Left()) == Tile::ClosedDoor);
+			}
+			THEN("The other 3 sides and the corners are walls")
+			{
+				REQUIRE(gm.GetTile(buildPoint.Up()) == Tile::ClosedDoor);
+				REQUIRE(gm.GetTile(buildPoint.Right()) == Tile::Wall);
+				REQUIRE(gm.GetTile(buildPoint.Down()) == Tile::Wall);
+				
+				REQUIRE(gm.GetTile(buildPoint.Left().Down()) == Tile::Wall);
+				REQUIRE(gm.GetTile(buildPoint.Right().Down()) == Tile::Wall);
+				REQUIRE(gm.GetTile(buildPoint.Left().Up()) == Tile::Wall);
+				REQUIRE(gm.GetTile(buildPoint.Right().Up()) == Tile::Wall);
+			}
+	    }
+	}
+}
